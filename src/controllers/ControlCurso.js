3609faa3fd4abe1a7931d6a348f58a90adecab4c
luisreis-cell <luis.reis@aluno.ifsp.edu.br>
2025-10-21 17:22:04 +0000

    teste2

--- src/controllers/ControlCurso.js
+++ src/controllers/ControlCurso.js
@@ -8,3 +8,18 @@ module.exports = {
   }
 };
 
+const Course = require('../models/course');
+const db = require('../config/db');
+
+module.exports = {
+  async remove(req, res) {
+    const [matriculas] = await db.execute(
+      'SELECT * FROM Matriculas WHERE curso_id = ?', [req.params.id]
+    );
+    if (matriculas.length > 0) {
+      return res.render('courses/list', { error: 'Não é possível excluir curso com alunos matriculados.', courses: await Course.list() });
+    }
+    await Course.remove(req.params.id);
+    res.redirect('/courses');
+  }
+};
