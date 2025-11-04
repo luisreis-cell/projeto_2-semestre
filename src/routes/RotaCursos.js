c62e53aa24962d40d0e2b04e67fc62704763bc64
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-04 11:54:35 -0300

    Update RotaCursos.js

--- src/routes/RotaCursos.js
+++ src/routes/RotaCursos.js
@@ -2,23 +2,11 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
 
-// Cadastro de curso (formulário)
 router.get('/new', courseController.showForm);
-// Cadastro de curso (processar)
 router.post('/save', courseController.create);
-
-// Listar cursos
 router.get('/', courseController.list);
-
-// Editar curso (formulário)
 router.get('/:id/edit', courseController.showForm);
-// Editar curso (processar)
 router.post('/save', courseController.createOrUpdate);
-
-// Excluir curso (com verificação de alunos associados)
 router.get('/:id/delete', courseController.remove);
-
-// Exibir detalhes do curso
 router.get('/:id', courseController.details);
-
 module.exports = router;
