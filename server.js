dc1f31b2e734934f6c4a09e7a5ab6a3d1e46909d
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-11 11:28:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
