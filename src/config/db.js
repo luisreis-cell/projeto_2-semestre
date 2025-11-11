7beadf939b96da7669301672f04d1da96ba8ecc3
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-11 14:49:49 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: 'localhost',  
+  user: 'app_user',  
+  password: 'S3nh@Aplicacao',
+  database: 'SistemaEscolar'  
+});
+
+module.exports = pool;
 
