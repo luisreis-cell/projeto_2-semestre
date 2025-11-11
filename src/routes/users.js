24313b92ed472230edaf855cbb2a9c5a7d1434a7
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-11 15:01:35 -0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/register', userController.showRegister);
-router.post('/register', userController.register);
 router.get('/', userController.listUsers);
-router.get('/:id/edit', userController.editForm);
-router.post('/update', userController.update);
-router.get('/profile', userController.profile);
-router.get('/:id/delete', userController.remove);
+router.get('/:id', userController.getUserById);
+router.post('/register', userController.createUser);
+router.get('/:id/delete', userController.deleteUser);
 
 module.exports = router;
