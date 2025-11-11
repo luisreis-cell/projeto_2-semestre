6a9829b2ee754120ee9af5b4d16bba8ec3437567
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-11 14:54:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ const path = require('path');
 const session = require('express-session'); 
 const app = express();
 
-// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Configuração de sessão (necessário para login)
 app.use(session({
-  secret: 'sua_chave_secreta', // troque por uma chave segura
+  secret: 'sua_chave_secreta', 
   resave: false,
   saveUninitialized: false
 }));
 
-// Views e arquivos estáticos
-// Ajuste para views dentro de src
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// Rotas
 const usersRouter = require('./src/routes/users');
 const authRouter = require('./src/routes/auth');
 const studentsRouter = require('./src/routes/students');
@@ -31,7 +26,6 @@ app.use('/auth', authRouter);
 app.use('/students', studentsRouter);
 app.use('/courses', coursesRouter);
 
-// Rota raiz
 app.get('/', (req, res) => {
   res.render('index', { userName: req.session.userName });
 });
