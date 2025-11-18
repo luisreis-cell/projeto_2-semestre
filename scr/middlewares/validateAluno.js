4342601ca302c14f01a01453bf9f8689e7cac50b
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-18 14:47:53 -0300

    Delete scr/middlewares/validateAluno.js

--- scr/middlewares/validateAluno.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    const { nome, email } = req.body;
-    if (!nome || !email) {
-        return res.status(400).send("Nome e email são obrigatórios");
-    }
-    next();
-};
