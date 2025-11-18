63b4b7ff6c220660dd117a255e775ad66652dc39
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-11-18 14:33:01 -0300

    Create usuarioController.js

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,33 @@
+const usuarioService = require("../services/usuarioService");
+
+module.exports = {
+    mostrarLogin: (req, res) => {
+        res.render("login");
+    },
+
+    login: async (req, res, next) => {
+        try {
+            const { email, senha } = req.body;
+            const usuario = await usuarioService.autenticar(email, senha);
+            if (!usuario) return res.status(401).send("Credenciais inválidas");
+            
+            req.session.user = usuario;
+            res.redirect("/alunos");
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    cadastro: (req, res) => {
+        res.render("cadastro");
+    },
+
+    criar: async (req, res, next) => {
+        try {
+            await usuarioService.criarUsuario(req.body);
+            res.redirect("/usuarios/login");
+        } catch (e) {
+            next(e);
+        }
+    }
+};
