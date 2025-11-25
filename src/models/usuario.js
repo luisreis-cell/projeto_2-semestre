024f17a27c7938d6d428542c56e3b7cbb8619f59
pelepretao <lp2178523@gmail.com>
2025-11-25 11:36:54 -0300

    Update usuario.js

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,10 +1,4 @@
 const { DataTypes } = require("sequelize");
-const db = require("../config/db");
 
-const Usuario = db.define("Usuario", {
-    nome: DataTypes.STRING,
-    email: { type: DataTypes.STRING, unique: true },
-    senha: DataTypes.STRING,
-});
 
 module.exports = Usuario;
