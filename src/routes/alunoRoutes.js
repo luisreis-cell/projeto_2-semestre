106107e29999e0389f42f1877bed48c55bb99643
Nickolas012 <nicolasinsfran17@gmail.com>
2025-11-25 11:39:11 -0300

    Update alunoRoutes.js

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const alunoController = require('../controllers/alunoController');
+
+router.get('/', alunoController.list);
+router.post('/add', alunoController.create);
+router.put('/edit/:id', alunoController.update);
+router.delete('/delete/:id', alunoController.delete);
+module.exports = router;
