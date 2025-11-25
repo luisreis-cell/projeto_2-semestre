f9799fd4c2484bbbbb894e6d7656f0614c4dcbe8
pelepretao <lp2178523@gmail.com>
2025-11-25 11:38:09 -0300

    Remove auth middleware from alunoRoutes
    Removed authentication middleware from alunoRoutes.

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const alunoController = require("../controllers/alunoController");
-const auth = require("../middlewares/authMiddleware");
 
-router.get("/", auth, alunoController.listar);
-router.post("/novo", auth, alunoController.criar);
+
+router.get("/",  alunoController.listar);
+router.post("/novo",  alunoController.criar);
 
 module.exports = router;
