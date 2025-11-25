2c7e54cac358338c0fe837b130a95d79658e83ec
Nickolas012 <nicolasinsfran17@gmail.com>
2025-11-25 11:28:29 -0300

    Add update and delete methods to alunoController

--- src/controllers/alunoController.js
+++ src/controllers/alunoController.js
@@ -13,4 +13,18 @@ exports.list = (req, res) => {
         .catch(err => res.status(500).send(err));
 };
 
+exports.update = (req, res) => {
+    const { nome, curso_id } = req.body;
+    const { id } = req.params;
+    Aluno.update(id, nome, curso_id)
+        .then(result => res.redirect('/alunos'))
+        .catch(err => res.status(500).send(err));
+};
+
+exports.delete = (req, res) => {
+    const { id } = req.params;
+    Aluno.delete(id)
+        .then(result => res.redirect('/alunos'))
+        .catch(err => res.status(500).send(err));
+};
 
