a6716ff4c9faf5210a22afb50eb4ec53ddc4a2a7
pelepretao <lp2178523@gmail.com>
2025-11-25 12:10:30 -0300

    Implement CRUD operations for cursos

--- src/models/curso.js
+++ src/models/curso.js
@@ -1,3 +1,26 @@
-const { DataTypes } = require("sequelize");
+let cursos = [];
+let idAtual = 1;
 
-module.exports = Curso;
+module.exports = {
+    listar() {
+        return cursos;
+    },
+    buscarPorId(id) {
+        return cursos.find(c => c.id == id);
+    },
+    criar({ nome, carga_horaria }) {
+        const novo = { id: idAtual++, nome, carga_horaria };
+        cursos.push(novo);
+        return novo;
+    },
+    editar(id, { nome, carga_horaria }) {
+        const curso = cursos.find(c => c.id == id);
+        if (!curso) return null;
+        curso.nome = nome;
+        curso.carga_horaria = carga_horaria;
+        return curso;
+    },
+    deletar(id) {
+        cursos = cursos.filter(c => c.id != id);
+    }
+};
