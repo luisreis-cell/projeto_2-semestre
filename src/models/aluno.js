796fad3b8ba4909fb3afd3f867750ab30e9d6dde
Nickolas012 <nicolasinsfran17@gmail.com>
2025-11-25 11:38:10 -0300

    Implement CRUD operations for Aluno model

--- src/models/aluno.js
+++ src/models/aluno.js
@@ -1,3 +1,26 @@
-const { DataTypes } = require("sequelize");
+
+const db = require('../config/db');
+
+const Aluno = {
+    create: (nome, curso_id) => {
+        const query = 'INSERT INTO alunos (nome, curso_id) VALUES (?, ?)';
+        return db.promise().execute(query, [nome, curso_id]);
+    },
+
+    findAll: () => {
+        const query = 'SELECT alunos.nome, cursos.nome AS curso FROM alunos JOIN cursos ON alunos.curso_id = cursos.id';
+        return db.promise().query(query);
+    },
+
+    update: (id, nome, curso_id) => {
+        const query = 'UPDATE alunos SET nome = ?, curso_id = ? WHERE id = ?';
+        return db.promise().execute(query, [nome, curso_id, id]);
+    },
+
+    delete: (id) => {
+        const query = 'DELETE FROM alunos WHERE id = ?';
+        return db.promise().execute(query, [id]);
+    }
+};
 
 module.exports = Aluno;
