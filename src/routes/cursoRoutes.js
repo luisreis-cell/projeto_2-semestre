3c54e66a6223cd352eeffa5d28228abbbb1f7295
pelepretao <lp2178523@gmail.com>
2025-11-25 11:56:31 -0300

    Refactor cursoRoutes to add edit and delete routes

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -1,8 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const cursoController = require("../controllers/cursoController");
+const cursoController = require('../controllers/cursoController');
 
-router.get("/", cursoController.listar);
-router.post("/novo", cursoController.criar);
+router.get('/', cursoController.listar);
+router.get('/novo', cursoController.formNovo);
+router.post('/novo', cursoController.criar);
+router.get('/editar/:id', cursoController.formEditar);
+router.post('/editar/:id', cursoController.editar);
+router.get('/deletar/:id', cursoController.deletar);
 
 module.exports = router;
