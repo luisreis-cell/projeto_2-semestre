07fd8e6a93e9b12865b39c6da4d3dc20801f28ad
pelepretao <lp2178523@gmail.com>
2025-11-25 12:11:11 -0300

    Implement user management functions in usuario.js

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,4 +1,19 @@
-const { DataTypes } = require("sequelize");
+let usuarios = [];
+let idAtual = 1;
 
-
-module.exports = Usuario;
+module.exports = {
+    listar() {
+        return usuarios;
+    },
+    buscarPorId(id) {
+        return usuarios.find(u => u.id == id);
+    },
+    buscarPorEmail(email) {
+        return usuarios.find(u => u.email === email);
+    },
+    criar({ nome, email, senha }) {
+        const novo = { id: idAtual++, nome, email, senha };
+        usuarios.push(novo);
+        return novo;
+    }
+};
