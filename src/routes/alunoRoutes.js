66566a32bf2822875ecd29459a866a974ca73064
Lucas Costa Pires <lp2178523@gmail.com>
2025-11-25 14:32:15 -0300

    Update alunoRoutes.js

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const alunoController = require('../controllers/alunoController');
-router.get('/', alunoController.listar);   
-router.get('/novo', alunoController.formNovo);   
-router.post('/novo', alunoController.criar);   
-router.get('/editar/:id', alunoController.formEditar);
-router.post('/editar/:id', alunoController.editar);   
-router.get('/deletar/:id', alunoController.deletar);   
+
+router.get('/', alunoController.listar);
+router.get('/novo', alunoController.formNovo);
+router.post('/novo', alunoController.criar);
+router.get('/:id/editar', alunoController.formEditar);
+router.post('/:id/editar', alunoController.editar);
+router.get('/:id/deletar', alunoController.deletar);
 
 module.exports = router;
