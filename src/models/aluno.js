98ea6e5a3a00f60fe4702a56745ac5744dfbef1a
pelepretao <lp2178523@gmail.com>
2025-11-25 11:37:29 -0300

    Delete Aluno model from aluno.js
    Removed Aluno model definition and database import.

--- src/models/aluno.js
+++ src/models/aluno.js
@@ -1,10 +1,3 @@
 const { DataTypes } = require("sequelize");
-const db = require("../config/db");
-
-const Aluno = db.define("Aluno", {
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cursoId: DataTypes.INTEGER
-});
 
 module.exports = Aluno;
