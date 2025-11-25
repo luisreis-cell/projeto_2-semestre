ff9756e4624d49851bbe4328d03955584d3bbb8b
pelepretao <lp2178523@gmail.com>
2025-11-25 12:07:16 -0300

    Refactor cursoController to use Curso model methods

--- src/controllers/cursoController.js
+++ src/controllers/cursoController.js
@@ -1,20 +1,39 @@
-const cursoService = require("../services/cursoService");
+const Curso = require('../models/curso');
 
 module.exports = {
-    listar: async (req, res, next) => {
-        try {
-            const cursos = await cursoService.listar();
-            res.render("cursos", { cursos });
-        } catch (e) {
-            next(e);
-        }
+
+    async listar(req, res) {
+        const cursos = await Curso.listar();
+        res.render('curso/listar', { cursos });
     },
-    criar: async (req, res, next) => {
-        try {
-            await cursoService.criar(req.body);
-            res.redirect("/cursos");
-        } catch (e) {
-            next(e);
-        }
+
+    async formNovo(req, res) {
+        res.render('curso/novo');
+    },
+
+    async criar(req, res) {
+        const { nome, carga_horaria } = req.body;
+
+        await Curso.criar({ nome, carga_horaria });
+
+        res.redirect('/curso');
+    },
+
+    async formEditar(req, res) {
+        const curso = await Curso.buscarPorId(req.params.id);
+        res.render('curso/editar', { curso });
+    },
+
+    async editar(req, res) {
+        const { nome, carga_horaria } = req.body;
+
+        await Curso.editar(req.params.id, { nome, carga_horaria });
+
+        res.redirect('/curso');
+    },
+
+    async deletar(req, res) {
+        await Curso.deletar(req.params.id);
+        res.redirect('/curso');
     }
 };
