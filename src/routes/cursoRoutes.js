cf83d895fe0bd601944f5bb78e67a849c9b77a8b
pelepretao <lp2178523@gmail.com>
2025-11-25 11:38:58 -0300

    Remove auth middleware from cursoRoutes
    Removed authentication middleware from cursoRoutes.

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const cursoController = require("../controllers/cursoController");
-const auth = require("../middlewares/authMiddleware");
 
-router.get("/", auth, cursoController.listar);
-router.post("/novo", auth, cursoController.criar);
+router.get("/", cursoController.listar);
+router.post("/novo", cursoController.criar);
 
 module.exports = router;
