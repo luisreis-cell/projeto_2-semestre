f00956db74d881a870245e49b6715e40bad2246f
Nickolas012 <nicolasinsfran17@gmail.com>
2025-11-25 11:41:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-require("dotenv").config();
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const sessionConfig = require('./config/session');
+const alunoRoutes = require('./routes/alunoRoutes');
+const cursoRoutes = require('./routes/cursoRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 
-const alunoRoutes = require("./routes/alunoRoutes");
-const cursoRoutes = require("./routes/cursoRoutes");
-const usuarioRoutes = require("./routes/usuarioRoutes");
+sessionConfig(app);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(session);
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use("/alunos", alunoRoutes);
-app.use("/cursos", cursoRoutes);
-app.use("/usuarios", usuarioRoutes);
-
-app.use(errorHandler);
-
-app.listen(process.env.PORT || 3000, () =>
-    console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`)
-);
