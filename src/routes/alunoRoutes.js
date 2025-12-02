341e3fe1646ed72e066026cfc79cd497c8934d36
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-12-02 16:23:57 -0300

    Refactor alunoRoutes to remove authentication middleware

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const alunoController = require('../controllers/alunoController');
-const { verificarAutenticacao } = require('../middleware/auth');
 
-router.get('/', verificarAutenticacao, alunoController.listarTodos);
-router.get('/novo', verificarAutenticacao, alunoController.novoFormulario);
-router.post('/novo', verificarAutenticacao, alunoController.salvar);
-router.get('/:id/editar', verificarAutenticacao, alunoController.editarFormulario);
-router.post('/:id/editar', verificarAutenticacao, alunoController.atualizar)
-router.get('/:id/excluir', verificarAutenticacao, alunoController.excluir);
+router.get('/aluno', alunoController.listarTodos);
+router.get('/aluno/novo', alunoController.novoFormulario);
+router.post('/aluno', alunoController.salvar);
+router.get('/aluno/editar/:id', alunoController.editarFormulario);
+router.post('/aluno/:id', alunoController.atualizar);
+router.post('/aluno/deletar/:id', alunoController.excluir);
+
 module.exports = router;
