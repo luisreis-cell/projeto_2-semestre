fe795cc62cac711f368ea1a6e4c605a34e6c492d
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-12-02 14:06:02 -0300

    Refactor alunoController methods and error handling

--- src/controllers/alunoController.js
+++ src/controllers/alunoController.js
@@ -1,44 +1,44 @@
 const Aluno = require('../models/aluno');
-const Curso = require('../models/curso'); 
+const Curso = require('../models/curso');
 
 module.exports = {
 
     async listar(req, res) {
         try {
-            const alunos = await Aluno.listarComCurso(); // <-- JOIN
-            res.render('aluno/listar', { alunos });
-        } catch (erro) {
+            const alunosComCursos = await Aluno.listarComCurso();
+            res.render('aluno/listar', { alunos: alunosComCursos });
+        } catch (error) {
             res.status(500).send("Erro ao listar alunos.");
         }
     },
 
-    async formNovo(req, res) {
+    async mostrarFormularioNovo(req, res) {
         try {
-            const cursos = await Curso.listar();
-            res.render('aluno/novo', { cursos });
-        } catch (erro) {
-            res.status(500).send("Erro ao abrir formulário de aluno.");
+            const listaCursos = await Curso.listar();
+            res.render('aluno/novo', { cursos: listaCursos });
+        } catch (error) {
+            res.status(500).send("Erro ao abrir formulário de novo aluno.");
         }
     },
 
     async criar(req, res) {
         try {
             const { nome, idade, curso_id } = req.body;
-
             await Aluno.criar({ nome, idade, curso_id });
-
             req.flash('success', 'Aluno criado com sucesso.');
             res.redirect('/aluno');
-        } catch (erro) {
+        } catch (error) {
             res.status(500).send("Erro ao criar aluno.");
         }
     },
-    async formEditar(req, res) {
+
+    async mostrarFormularioEditar(req, res) {
         try {
-            const aluno = await Aluno.buscarPorId(req.params.id);
-            const cursos = await Curso.listar(); // mantém select preenchido
-            res.render('aluno/editar', { aluno, cursos });
-        } catch (erro) {
+            const alunoParaEditar = await Aluno.buscarPorId(req.params.id);
+            const listaCursos = await Curso.listar();
+
+            res.render('aluno/editar', { aluno: alunoParaEditar, cursos: listaCursos });
+        } catch (error) {
             res.status(500).send("Erro ao abrir formulário de edição.");
         }
     },
@@ -46,12 +46,10 @@ module.exports = {
     async editar(req, res) {
         try {
             const { nome, idade, curso_id } = req.body;
-
             await Aluno.editar(req.params.id, { nome, idade, curso_id });
-
             req.flash('success', 'Aluno atualizado com sucesso.');
             res.redirect('/aluno');
-        } catch (erro) {
+        } catch (error) {
             res.status(500).send("Erro ao editar aluno.");
         }
     },
@@ -59,9 +57,9 @@ module.exports = {
     async deletar(req, res) {
         try {
             await Aluno.deletar(req.params.id);
-            req.flash('success', 'Aluno excluído.');
+            req.flash('success', 'Aluno excluído com sucesso.');
             res.redirect('/aluno');
-        } catch (erro) {
+        } catch (error) {
             res.status(500).send("Erro ao excluir aluno.");
         }
     }
