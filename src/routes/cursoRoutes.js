a2c5b273225cce6eda135be9f2916729f1d9bbe5
Gouvea14 <guilherme.gouvea@aluno.ifsp.edu.br>
2025-12-02 16:24:42 -0300

    Refactor cursoRoutes to simplify routes and remove auth

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -1,43 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const cursoController = require('../controllers/cursoController');
-const { verificarAutenticacao, verificarAdmin } = require('../middleware/auth');
 
-router.get('/', cursoController.listarTodos);
-
-router.get(
-  '/novo',
-  verificarAutenticacao,
-  verificarAdmin,
-  cursoController.novoFormulario
-);
-
-router.post(
-  '/novo',
-  verificarAutenticacao,
-  verificarAdmin,
-  cursoController.salvar
-);
-
-router.get(
-  '/:id/editar',
-  verificarAutenticacao,
-  verificarAdmin,
-  cursoController.editarFormulario
-);
-
-router.post(
-  '/:id/editar',
-  verificarAutenticacao,
-  verificarAdmin,
-  cursoController.atualizar
-);
-
-router.get(
-  '/:id/excluir',
-  verificarAutenticacao,
-  verificarAdmin,
-  cursoController.excluir
-);
+router.get('/', cursoController.listar);            
+router.get('/novo', cursoController.formNovo);        
+router.post('/', cursoController.criar);             
+router.get('/editar/:id', cursoController.editarForm);
+router.post('/:id', cursoController.atualizar);       
+router.post('/deletar/:id', cursoController.remover); 
 
 module.exports = router;
