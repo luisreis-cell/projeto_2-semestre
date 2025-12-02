73e0264dd31e78be770b566d3702a4de93995b9d
Lucas Costa Pires <lp2178523@gmail.com>
2025-12-02 11:36:57 -0300

    Update usuarioRoutes.js

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -4,10 +4,8 @@ const usuarioController = require('../controllers/usuarioController');
 
 router.get('/login', usuarioController.formLogin);
 router.post('/login', usuarioController.login);
-
 router.get('/cadastro', usuarioController.formCadastro);
 router.post('/cadastro', usuarioController.cadastrar);
-
 router.get('/logout', usuarioController.logout);
 
 module.exports = router;
