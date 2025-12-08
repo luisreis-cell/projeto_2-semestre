666b80732bb4ba283b5e6805ac31abaab403b274
Nickolas012 <nicolasinsfran17@gmail.com>
2025-12-08 17:30:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,25 +8,20 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-// Ajuste para usar as views dentro de src/views
 app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Debug: mostrar diretório de views e existência do template de cursos
 const fs = require('fs');
 const testePath = path.join(app.get('views'), 'curso', 'listar.ejs');
 console.log('Views directory:', app.get('views'));
 console.log('Teste arquivo curso/listar.ejs existe?', fs.existsSync(testePath), testePath);
 
-// Session middleware (uses src/config/session.js)
 require('./src/config/session')(app);
 
-// Expor usuário da sessão para as views
 app.use((req, res, next) => {
     res.locals.usuario = req.session ? req.session.usuario : null;
     next();
 });
 
-// connect-flash setup
 const flash = require('connect-flash');
 app.use(flash());
 app.use((req, res, next) => {
@@ -34,12 +29,10 @@ app.use((req, res, next) => {
     next();
 });
 
-// Rota raiz: redireciona para login
 app.get('/', (req, res) => {
     return res.render('index');
 });
 
-// Routes
 const alunoRoutes = require('./src/routes/alunoRoutes');
 const cursoRoutes = require('./src/routes/cursoRoutes');
 const usuarioRoutes = require('./src/routes/usuarioRoutes');
@@ -50,7 +43,6 @@ app.use('/aluno', alunoRoutes);
 app.use('/curso', cursoRoutes);
 app.use('/painel', painelRoutes);
 
-// Acesso negado
 app.get('/acesso-negado', (req, res) => res.render('acesso-negado'));
 
 const PORT = process.env.PORT || 3000;
